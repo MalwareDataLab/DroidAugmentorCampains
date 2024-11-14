a02322ab4e05f882d115f8392dd465430fd67cb8
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-11-14 20:26:35 -0300

    Create run_reproduce_sf24_docker.sh

---
+++ run_reproduce_sf24_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#USER_ID=$(id -u $USER)
+if docker info >/dev/null 2>&1; then
+	DIR=$(readlink -f shared)
+	if [ -z "$(docker images -q  wrseg/droidaugmento:latest 2> /dev/null)" ]; then
+	docker build -t wrseg/droidaugmentor:latest . 
+	fi
+	docker run -it --name=/DroidAugmentor-$RANDOM -v $DIR:/DroidAugmentor/shared -e DISPLAY=unix$DISPLAY  wrseg/droidaugmento:latest pipenv run python3 run_campaign.py
+	#ls shared/outputs/
+else
+    echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
+fi
