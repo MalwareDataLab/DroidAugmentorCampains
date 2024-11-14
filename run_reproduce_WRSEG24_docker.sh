fd39959a8fb459f41e9e434668896f3d01dcbdf3
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-11-14 20:36:19 -0300

    Update run_reproduce_WRSEG24_docker.sh

--- run_reproduce_WRSEG24_docker.sh
+++ run_reproduce_WRSEG24_docker.sh
@@ -5,7 +5,7 @@ if docker info >/dev/null 2>&1; then
 	if [ -z "$(docker images -q  wrseg/droidaugmento:latest 2> /dev/null)" ]; then
 	docker build -t wrseg/droidaugmentor:latest . 
 	fi
-	docker run -it --name=/DroidAugmentor-$RANDOM -v $DIR:/DroidAugmentor/shared -e DISPLAY=unix$DISPLAY  wrseg/droidaugmento:latest pipenv run python3 run_campaign.py
+	docker run -it --name=/DroidAugmentor-$RANDOM -v $DIR:/DroidAugmentor/shared -e DISPLAY=unix$DISPLAY  wrseg/droidaugmento:latest /DroidAugmentor/run_reproduce_WRSEG24_venv.sh
 	#ls shared/outputs/
 else
     echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
