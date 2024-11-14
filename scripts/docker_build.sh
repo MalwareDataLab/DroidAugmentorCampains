4659d4119b37be41e4eb3d3a40074e2025259fb1
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2024-11-14 20:17:28 -0300

    Update docker_build.sh

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if docker info >/dev/null 2>&1; then
-	docker build -t sf24/malsyngen:latest . 
+	docker build -t wrseg/droidaugmentor:latest . 
 else
     echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
 fi
